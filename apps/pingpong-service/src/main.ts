35c98f02c326a9d64a5b90f3b41347fe1db52ca8
Melbromss <melbromss@gmail.com>
2025-02-15 23:07:38 +0700

    [fixed] add database connect to all services in main.ts

--- apps/pingpong-service/src/main.ts
+++ apps/pingpong-service/src/main.ts
@@ -8,11 +8,11 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // Create MySQL connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'admin_db',
+  port: Number(process.env.DB_PORT) || 3307,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
