c13fcc39cf0067888b80bcae049b7660d41dfb13
ppete10 <ppete.st10@gmail.com>
2025-02-24 11:40:04 +0700

    [add] - test socket io

--- apps/basketball-service/src/app/app.ts
+++ apps/basketball-service/src/app/app.ts
@@ -1,11 +1,27 @@
 import express from 'express';
 import * as path from 'path';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import basketballRoutes from '../routes/basketball.route';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.ALLOWED_ORIGINS || '*',
+  },
+});
 
 app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, '../../assets')));
 app.use('/api/basketball', basketballRoutes);
 
-export default app;
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+export { app, server, io };
--- apps/basketball-service/src/routes/basketball.route.ts
+++ apps/basketball-service/src/routes/basketball.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { getScoreboard, getSchedule } from '../controllers/basketball.controller';
+import { io } from '../app/app';
 
 const router = Router();
 
-router.get('/scoreboard', getScoreboard)
+router.get('/scoreboard', async (req, res) => {
+    await getScoreboard(req, res);
+    io.emit('updateScoreboard', 'New scoreboard data available');
+  });
 router.get('/schedule', getSchedule)
 
 export default router;
