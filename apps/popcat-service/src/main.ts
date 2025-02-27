7b46b71522e1124c5a2c6434b4de39d3fefe0a32
BoolinBood <napat.jins@mail.kmutt.ac.th>
2025-02-27 17:58:07 +0700

    🔧(socket): change configurations

--- apps/popcat-service/src/main.ts
+++ apps/popcat-service/src/main.ts
@@ -4,21 +4,28 @@ import { Server } from 'socket.io';
 import { connectRedis } from './config/redis';
 import { setupWebSocket } from './websocket/websocket.controller';
 
+import cors from 'cors';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: process.env.ALLOWED_ORIGINS || "*",
+    origin: process.env.ALLOWED_ORIGINS || '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true, // If you need cookies or credentials in your requests
     },
+    path: '/popcat/socket/',
 });
-const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 8086;
+
+app.use(cors());
 app.use(express.json());
 
 connectRedis().then(() => {
     setupWebSocket(io);
     server.listen(PORT, () => {
-        console.log(`WebSocket server running on port ${PORT}`);
+    console.log(`WebSocket server running on port ${PORT}`);
     });
-})
+});
